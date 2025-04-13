89ecab2d97a4fc6c9bc73a8781959bd52eaa0d5c
JustAdi10 <aditya.pawar23624@gmail.com>
2025-04-14 05:21:49 +0530

    Monad tweaks rem:optimise contract

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      metadata: {
+        bytecodeHash: "none", // disable ipfs
+        useLiteralContent: true, // use source code
+      },
+    },
+  },
+  networks: {
+    monadTestnet: {
+      url: "https://testnet-rpc.monad.xyz",
+      chainId: 10143,
+    },
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify-api-monad.blockvision.org",
+    browserUrl: "https://testnet.monadexplorer.com",
+  },
+  // To avoid errors from Etherscan
+  etherscan: {
+    enabled: false,
+  },
 };
 
 export default config;
